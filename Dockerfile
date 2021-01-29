88c19757e258d93a4b1631177f79f2b1e12b154e
Wade Williams <wade@wadewilliams.com>
2021-01-29 14:56:01 -0700

    License, Dockerfile, setup.py

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.8.7-slim-buster
+
+RUN apt-get update -y && \
+    apt-get install -y git && \
+    pip3 install glances
+
+WORKDIR /app
+
+#COPY Pipfile Pipfile
+#COPY Pipfile.lock Pipfile.lock
+RUN pip3 install pipenv
+ 
+#RUN pipenv install --dev --deploy --python 3.8.7
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="warf",
+    version="0.1.0",
+    description="Wade's Own Rest Framework: A more djangonic approach",
+    long_description="",
+    keywords="django, rest, framework",
+    author="Wade Williams",
+    author_email="wade@wadewilliams.com",
+    url="https://github.com/flowcanon/worf",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "Django>=3.0.0",
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ]
+)
