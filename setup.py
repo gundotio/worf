423ff80710d4b4406a4043ebd59b2c7dd2066583
Wade Williams <wade@wadewilliams.com>
2021-03-30 14:34:32 -0600

    clocignore, bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="worf",
-    version="0.1.2",
+    version="0.1.3",
     description="Wade's Own Rest Framework: A more djangonic approach",
     long_description="",
     keywords="django, rest, framework",
