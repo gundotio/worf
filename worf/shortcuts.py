cf7d91b782c3fac6ef3623c92508b287f7e159b4
Wade Williams <wade@wadewilliams.com>
2021-03-31 15:55:31 -0600

    black

--- worf/shortcuts.py
+++ worf/shortcuts.py
@@ -8,5 +8,7 @@ def get_instance_or_http404(model, *args, **kwargs):
         return getattr(getattr(model, "objects"), "get")(*args, **kwargs)
     except model.DoesNotExist:
         if settings.DEBUG:
-            raise Http404("No {} matches the given query.".format(model._meta.object_name))
+            raise Http404(
+                "No {} matches the given query.".format(model._meta.object_name)
+            )
         raise HTTP404()
