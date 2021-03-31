0175cf0da21106f0949c7ae0372b5b9be5ae8894
Wade Williams <wade@wadewilliams.com>
2021-03-31 15:42:43 -0600

    Fall back to django Http404 for DEBUG

--- worf/shortcuts.py
+++ worf/shortcuts.py
@@ -1,11 +1,12 @@
 from django.conf import settings
+from django.http import Http404
 from worf.exceptions import HTTP404
 
 
-def get_instance_or_http404(model, **kwargs):
+def get_instance_or_http404(model, *args, **kwargs):
     try:
-        return getattr(getattr(model, "objects"), "get")(**kwargs)
-    except model.DoesNotExist as e:
+        return getattr(getattr(model, "objects"), "get")(*args, **kwargs)
+    except model.DoesNotExist:
         if settings.DEBUG:
-            raise e
-        raise HTTP404
+            raise Http404("No {} matches the given query.".format(model._meta.object_name))
+        raise HTTP404()
