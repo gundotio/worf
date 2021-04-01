29f85d6902a69bf3abbc357b7ac71a8dae7a37bb
Wade Williams <wade@wadewilliams.com>
2021-04-01 11:19:55 -0600

    Improve setup for pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,56 @@
+import os
 from setuptools import setup, find_packages
 
+
+def read(rel_path):
+    # type: (str) -> str
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    # type: (str) -> str
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.md")
+
 setup(
-    name="worf",
-    version="0.1.3",
-    description="Wade's Own Rest Framework: A more djangonic approach",
-    long_description="",
-    keywords="django, rest, framework",
-    author="Wade Williams",
     author_email="wade@wadewilliams.com",
-    url="https://github.com/gundotio/worf",
-    license="MIT",
-    packages=find_packages(),
-    zip_safe=False,
+    author="Wade Williams",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    description="Worf (Wade's Own Rest Framework): A more djangonic approach",
     install_requires=[
         "Django>=3.0.0",
     ],
     include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Framework :: Django",
-        "Environment :: Web Environment",
-    ],
+    keywords="django, rest, framework, api",
+    license="MIT",
+    long_description=long_description,
+    name="worf",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    url="https://github.com/gundotio/worf",
+    version=get_version("worf/__init__.py"),
+    zip_safe=False,
 )
