92e2cdd0fd80040affb2cb7982ed17e0e9123713
Wade Williams <wade@wadewilliams.com>
2021-04-22 13:52:37 -0600

    Naming things

--- tests/serializer_tests.py
+++ tests/serializer_tests.py
@@ -8,7 +8,9 @@ def test_user_get(client, django_user_model):
     assert deserialize(response)["username"] == "test"
 
     bundle = dict(username="testtest", email="something@example.com")
-    response = client.patch("/user/{}/".format(user.pk), bundle, content_type="application/json")
+    response = client.patch(
+        "/user/{}/".format(user.pk), bundle, content_type="application/json"
+    )
     assert response.status_code == 200
     assert deserialize(response)["username"] == "testtest"
     assert deserialize(response)["email"] == "something@example.com"
