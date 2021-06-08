0819f9743fdd908e5877b53a883d2577652283d4
Wade Williams <wade@wadewilliams.com>
2021-06-08 10:53:00 -0600

    Add shortcut for get_current_version

--- worf/shortcuts.py
+++ worf/shortcuts.py
@@ -1,5 +1,8 @@
+import subprocess
+
 from django.conf import settings
 from django.http import Http404
+from worf import __version__
 from worf.exceptions import HTTP404
 
 
@@ -12,3 +15,15 @@ def get_instance_or_http404(model, *args, **kwargs):
                 "No {} matches the given query.".format(model._meta.object_name)
             )
         raise HTTP404()
+
+
+def get_current_version():
+    try:
+        hash = (
+            subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+            .strip()
+            .decode()
+        )
+        return f"{__version__}@{hash}"
+    except:  # pragma: no cover # noqa E722 Dont crash for any reason whatsoever
+        return __version__
