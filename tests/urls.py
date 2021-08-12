335c7c9b6fe5b9038278c111d82a76f7e4f75bef
Regis Camimura <regiscamimura@gmail.com>
2021-08-12 12:42:36 -0300

    tests for array filters

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 
-from tests.views import DummyAPI, UserDetail, UserList
+from tests.views import DummyAPI, ProfileList, UserDetail, UserList
 
 urlpatterns = [
     path("", DummyAPI.as_view()),
     path("users/", UserList.as_view()),
+    path("profiles/", ProfileList.as_view()),
     path("users/<str:id>/", UserDetail.as_view()),
     path("<str:id>/", DummyAPI.as_view()),
 ]
