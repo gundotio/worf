3ff9fff206ae78e506117de2426f689c550b0c4f
Steve Lacey <steve@steve.ly>
2021-11-09 00:55:36 +0700

    Trash get_instance_or_http404

--- worf/shortcuts.py
+++ worf/shortcuts.py
@@ -1,20 +1,6 @@
 import subprocess
 
-from django.conf import settings
-from django.http import Http404
 from worf import __version__
-from worf.exceptions import HTTP404
-
-
-def get_instance_or_http404(model, *args, **kwargs):
-    try:
-        return getattr(getattr(model, "objects"), "get")(*args, **kwargs)
-    except model.DoesNotExist:
-        if settings.DEBUG:
-            raise Http404(
-                "No {} matches the given query.".format(model._meta.object_name)
-            )
-        raise HTTP404()
 
 
 def get_current_version():
