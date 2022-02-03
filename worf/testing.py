9a996e71d6c49b68ac89f68f3b73ed3facde906b
Steve Lacey <steve@steve.ly>
2022-02-03 17:18:40 +0700

    Support unique field validation

--- worf/testing.py
+++ worf/testing.py
@@ -7,25 +7,28 @@ JSON_CONTENT = "application/json"
 
 
 class ApiClient(Client):
-    def generic_multipart(self, method, path, data=None, content_type=None, **kwargs):
-        content_type = content_type or self._guess_content_type(data)
+    def generic(self, method, path, data=None, content_type=None, **kwargs):
+        content_type = content_type or guess_content_type(data)
         data = self._encode_multipart(data, content_type)
-        return self.generic(method, path, data, content_type, **kwargs)
+        return super().generic(method, path, data, content_type, **kwargs)
 
-    patch = partialmethod(generic_multipart, "PATCH")
-    post = partialmethod(generic_multipart, "POST")
-    put = partialmethod(generic_multipart, "PUT")
-
-    def _contains_files(self, data):
-        return any(isinstance(value, File) for value in data.values())
+    delete = partialmethod(generic, "DELETE")
+    patch = partialmethod(generic, "PATCH")
+    post = partialmethod(generic, "POST")
+    put = partialmethod(generic, "PUT")
 
     def _encode_multipart(self, data, content_type):
         post_data = self._encode_json({} if data is None else data, content_type)
         return self._encode_data(post_data, content_type)
 
-    def _guess_content_type(self, data):
-        return (
-            MULTIPART_CONTENT
-            if isinstance(data, dict) and self._contains_files(data)
-            else JSON_CONTENT
-        )
+
+def guess_content_type(data):
+    return MULTIPART_CONTENT if should_multipart(data) else JSON_CONTENT
+
+
+def has_files(data):
+    return any(isinstance(value, File) for value in data.values())
+
+
+def should_multipart(data):
+    return isinstance(data, dict) and has_files(data)
