fc046c39cdb78736d8f6665f14530e694923f85c
Steve Lacey <steve@steve.ly>
2022-07-13 17:01:53 +0100

    Support standalone DeleteAPIs

--- worf/views/delete.py
+++ worf/views/delete.py
@@ -1,7 +1,8 @@
+from worf.lookups import FindInstance
 from worf.views.base import AbstractBaseAPI
 
 
-class DeleteAPI(AbstractBaseAPI):
+class DeleteAPI(FindInstance, AbstractBaseAPI):
     def delete(self, *args, **kwargs):
         self.destroy(*args, **kwargs)
         return self.render_to_response("", 204)
