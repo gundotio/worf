6f9a73d0ac0b701175c157633974cbfe38f88797
Steve Lacey <steve@steve.ly>
2023-01-13 15:14:07 +0800

    Support passing generators to parametrize

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import pytest
 import pytest_factoryboy
 
@@ -6,9 +8,12 @@ raises = pytest.raises
 register = pytest_factoryboy.register
 
 
+@wraps(pytest.mark.kwparametrize)
 def parametrize(*args, **kwargs):
     if not args:
         return pytest.mark.kwparametrize(kwargs)
     if isinstance(args[0], dict):
         return pytest.mark.kwparametrize(*args, **kwargs)
+    if len(args) == 1 and not kwargs:
+        return pytest.mark.kwparametrize(*args[0])
     return pytest.mark.parametrize(*args, **kwargs)
